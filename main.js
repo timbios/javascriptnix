1d7110daca558981fa7132430d830a1e05e0145f
timbios <44220883+timbios@users.noreply.github.com>
2018-10-17 08:14:38 +0300

    update 2

--- main.js
+++ main.js
@@ -7,3 +7,11 @@ function reverseSTR(str) {
     return newStr;
 }
 reverseSTR('abcda');
+
+//ucFirst
+function ucFirst(str) {
+	if (!str) return str;
+	return str[0].toUpperCase() + str.slice(1);
+	}
+console.log(ucFirst('adsad'))
+
