f2cd17ae5b8e3b13a9f05fcd9a5007d40cc111b8
timbios <44220883+timbios@users.noreply.github.com>
2018-10-17 08:14:04 +0300

    update

---
+++ main.js
@@ -0,0 +1,9 @@
+// Reverse
+function reverseSTR(str) {
+    var newStr = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        newStr += str[i];
+    }
+    return newStr;
+}
+reverseSTR('abcda');
